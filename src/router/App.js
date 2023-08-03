3da260ce5759cefb5fe22b1d90e69eb72273e793
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-03 10:29:01 +0200

    Comment App.js

--- src/router/App.js
+++ src/router/App.js
@@ -1,6 +1,9 @@
+// file name: App.js
+
 import React from 'react';
 import '../styles/App.css';
 
+// Importiere Routing-Funktionen und Komponenten aus der react-router-dom Bibliothek
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -8,14 +11,16 @@ import {
   RouterProvider
 } from 'react-router-dom';
 
+// Importiere die Seitenkomponenten ShoppingList und Employees
 import ShoppingList from '../pages/ShoppingList';
 import Employees from '../pages/Employees';
 
+// Importiere eine Funktion, um Mitarbeiterdaten abzurufen
 import { getEmployees } from '../logic/RestHandler';
 
+// Erstelle einen Router mit zwei Routen: eine Hauptseite, die die Employees-Komponente anzeigt, und eine Einkaufslisten-Seite
 const router = createBrowserRouter(
   createRoutesFromElements(
-
     <Route path="/">
       <Route index element={<Employees />} loader={getEmployees} />
       <Route path='/shopping-list' element={<ShoppingList />} />
@@ -23,6 +28,7 @@ const router = createBrowserRouter(
   )
 );
 
+// Die Hauptfunktionskomponente der Anwendung, die den oben definierten Router bereitstellt
 function App() {
   return (<RouterProvider router={router} />);
 }
